9508e83db07e791122511ce8e87521b2781079c1
Rogerio Figueredo <rfigsousa@gmail.com>
2019-04-16 20:07:41 -0300

    Modificações gerais

--- opinion.py
+++ opinion.py
@@ -4,7 +4,9 @@ import simplejson as json
     
 '''
 class Opinion:
-    def __init__(self, id, autor , nome_objeto, data, estrelas, texto, thumbs_up, thumbs_down, recommend='', link=''):
+    def __init__(self, id, autor , nome_objeto, data, estrelas, texto, thumbs_up, thumbs_down, recommend='', link='', 
+        link_perfil_user='', collect_date='', see='', wsee='', favorite='', replies=''):
+
         self.id = id
         self.autor = autor
         self.nome_objeto = nome_objeto
@@ -15,6 +17,13 @@ class Opinion:
         self.thumbs_down = int(thumbs_down)
         self.recommend = recommend
         self.link = link
+        self.link_perfil_user = link_perfil_user
+        self.collect_date = collect_date
+        self.see = see
+        self.wsee = wsee
+        self.favorite = favorite
+        self.replies = replies
+
     
     def __repr__(self):
         return self.texto
@@ -24,5 +33,6 @@ class Opinion:
 
     def to_json(self):
         return json.dumps({"id":self.id, "autor":self.autor, "objeto":self.nome_objeto, "data":self.data, 
-        "estrelas":self.estrelas, "texto":self.texto, "likes":self.thumbs_up,'deslikes':self.thumbs_down, 
-        "recommend":self.recommend, "link":self.link})
+        "estrelas":self.estrelas, "texto":self.texto, "likes":self.thumbs_up,'unlikes':self.thumbs_down, 
+        "recommend":self.recommend, "link":self.link, "link_perfil_user":self.link_perfil_user, 
+        "collect_date":self.collect_date, "see":self.see, "want_see":self.wsee ,"favorite":self.favorite, "replies":self.replies})
--- show_opinions.py
+++ show_opinions.py
@@ -1,6 +1,6 @@
 import os
 
-raiz = 'reviews/'
+raiz = 'reviews_filmow/'
 
 for pasta in os.listdir(raiz):
         print(pasta + ': ' + str(len(os.listdir(raiz+pasta))))
